66ca54834d73f67617f74711dbc88b73d9538bd6
Bladimir Cielo <cieloblax@gmail.com>
2024-11-25 11:18:28 -0600

    Sangría en pedidoscontroller.php

--- app/Http/Controllers/pedidoscontroller.php
+++ app/Http/Controllers/pedidoscontroller.php
@@ -11,26 +11,25 @@ class pedidoscontroller extends controller {
 
     public function consultar(Request $request) {
         // Obtén las fechas de inicio y fin del request
-$fechaInicio = $request->input('fecha_inicio');
-$fechaFin = $request->input('fecha_fin');
+        $fechaInicio = $request->input('fecha_inicio');
+        $fechaFin = $request->input('fecha_fin');
 
-// Base de la consulta
-$ventas = \DB::table('ventas')
-    ->where('tipo', 'Domicilio') // Filtrando por tipo 'Domicilio'
-    ->select('id_venta', 'fecha_venta', 'total_venta', 'nombre_cliente', 'direccion_envio', 'costo_envio', 'hora_salida', 'estado') // Seleccionando las columnas
-    ->orderBy('fecha_venta', 'desc'); // Ordenando por fecha_venta en orden descendente
+        // Base de la consulta
+        $ventas = \DB::table('ventas')
+            ->where('tipo', 'Domicilio') // Filtrando por tipo 'Domicilio'
+            ->select('id_venta', 'fecha_venta', 'total_venta', 'nombre_cliente', 'direccion_envio', 'costo_envio', 'hora_salida', 'estado') // Seleccionando las columnas
+            ->orderBy('fecha_venta', 'desc'); // Ordenando por fecha_venta en orden descendente
 
-// Aplica el filtro de fecha solo si ambas fechas están presentes
-if ($fechaInicio && $fechaFin) {
-    // Verifica que las fechas están en el formato correcto antes de hacer la consulta
-    $ventas->whereBetween('fecha_venta', [$fechaInicio, $fechaFin]);
-}
+        // Aplica el filtro de fecha solo si ambas fechas están presentes
+        if ($fechaInicio && $fechaFin) {
+            // Verifica que las fechas están en el formato correcto antes de hacer la consulta
+            $ventas->whereBetween('fecha_venta', [$fechaInicio, $fechaFin]);
+        }
 
-// Ejecuta la consulta y obtiene los resultados
-$ventas = $ventas->get();
+        // Ejecuta la consulta y obtiene los resultados
+        $ventas = $ventas->get();
 
-// Devuelve la vista con los resultados
-return view('administracion.pedidos')->with('ventas', $ventas);
-
+        // Devuelve la vista con los resultados
+        return view('administracion.pedidos')->with('ventas', $ventas);
     }
 }
